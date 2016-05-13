4e1e2f0238e64b1588ac4f65ad7a8b5274c6fc6b
Benjie Songsong <benjiesongsong@gmail.com>
2016-05-13 13:50:29 -0400

    Finished submit function

--- src/js/controllers/appcontroller.js
+++ src/js/controllers/appcontroller.js
@@ -1,5 +1,9 @@
 import $ from 'jquery';
+
+
+
 export class AppController {
+
 
   constructor(contactForm, contactArea, contactListElem) {
     this.contactForm = contactForm;
@@ -7,19 +11,44 @@ export class AppController {
     this.contactList = contactListElem;
   }
 
-  runFunctions() {
-    this.submitContact();
-  }
 
-   submitContact() {
-    this.contactForm.on('submit', function (event) {
+  submitContact() {
+    this.contactForm.on('submit', (event) => {
       event.preventDefault();
-      let name = nameInput.value();
-      let photo = photoInput.value();
-      let phone = phoneInput.value();
-      let location = locationInput.value();
-      console.log(name, photo, phone, location);
+      let name          = this.contactForm.find('.nameInput').val();
+      let photo         = this.contactForm.find('.photoInput').val();
+      let phone         = this.contactForm.find('.phoneInput').val();
+      let location      = this.contactForm.find('.locationInput').val();
+
+      if (name.length < 1) {
+        alert('Please enter your name before submitting')
+      } else if (photo.length < 1) {
+        alert('Please enter your photo url before submitting')
+      } else if (phone.length < 1) {
+        alert('Please enter your phone number before submitting')
+      } else if (location.length < 1) {
+        alert('Please enter your city, state before submitting')
+      } else {
+        return $('.input').val('');
+      }
     });
    }
 
+
+  //  let name          = $('.nameInput').val();
+  //  let photo         = $('.photoInput').val();
+  //  let phone         = $('.phoneInput').val();
+  //  let location      = $('.locationInput').val();        let resetInput    = $('.input').val('');
+   //
+  //  submitContact(nameInput, photoInput, phoneInput, locationInput, resetInput) {
+  //   this.contactForm.on('submit', function (event) {
+  //     event.preventDefault();
+  //     let nameInput     = $('.nameInput').val();
+  //     let photoInput    = $('.photoInput').val();
+  //     let phoneInput    = $('.phoneInput').val();
+  //     let locationInput = $('.locationInput').val();
+  //     let resetInput = $('.input').val('');
+  //     console.log(nameInput, photoInput, phoneInput, locationInput, resetInput);
+  //   });
+  //  }
 }
--- src/js/main.js
+++ src/js/main.js
@@ -9,18 +9,16 @@ import { AppController } from './controllers/appcontroller';
 
 // variables to control HTML using jQuery
 let contactForm   = $('.contactForm');
-let nameInput     = $('.nameInput').val();
+let nameInput     = $('.nameInput');
 let photoInput    = $('.photoInput');
 let phoneInput    = $('.phoneInput');
 let locationInput = $('.locationInput');
 let submitContact = $('.submitContact');
 let contactList   = $('.contactList');
 
-
-console.log(nameInput);
 // instantiate the classes
 let contact = new Contact();
 let contactModel = new ContactModel();
 let appController = new AppController(contactForm, contactList, contactModel);
 console.log(appController);
-appController.runFunctions();
+appController.submitContact();
